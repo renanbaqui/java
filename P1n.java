4546a6d3df7c93b0f862d599f0c876d6495028c0
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-12 21:01:09 -0300

    Create P1n.java

---
+++ P1n.java
@@ -0,0 +1,33 @@
+// ************ falta completar
+import java.lang.Math.*;
+import java.util.Scanner;
+import java.util.Objects;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+
+   /* Scanner input = new Scanner (System.in);
+
+    //String line; //line = dataFile.readLine(); // Le uma linha por vez
+    private double peso, idade, altura;
+    int location, index;
+    char letter;
+
+    int[] letterCount = new int[26];
+
+    //Scanner input = new Scanner(System.in);
+    System.out.println ("Digite uma string: ");
+    String line = input.nextLine ();*/
+    System.out.println ("Seu resultado: ");
+    private static double calcula(float r)
+    {
+        double resultado = Math.PI * r * r
+        return resultado;
+    }
+    /*private static double calcula(float b, float a)
+    private static double calcula(float l1, floatl2, floatl3)*/
+    
+  }
+}
