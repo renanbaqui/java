6ef1bb6adca540694baf95a86a597ff7ecbc1d45
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 15:05:04 -0300

    Create P2nX.java

---
+++ P2n/P2nX.java
@@ -0,0 +1,59 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner input = new Scanner (System.in);
+	
+    	if (args.length==0)	// se nao houver argumentos
+        {
+          System.out.println("Numero de argumentos insuficiente");	
+        }
+        
+        if (args.length>=2) // se o numero de argumentos for igual ou maior a quatro
+        {
+          System.out.println("Numero de argumentos excessivo");
+        }	
+    	
+    	boolean valido = true;
+    
+        for (int i = 0; i < args.length; i++)	// loop de verificacao dos argumentos
+        {
+          boolean numeric = true;
+          try {	// verifica se e' numero
+            Double num = Double.parseDouble(args[i]);
+          } catch (NumberFormatException e) {
+            numeric = false;
+            valido = false;
+          }
+          
+          if (!numeric)	// se nao e' numero imprime a linha abaixo
+          {
+            System.out.println((i+1)+"o argumento, “"+ args[i] +"”, nao eh numero");
+          }
+        }
+    
+        if(valido){
+            
+            Angulo teste = new Angulo();
+            
+            double a1 = Double.parseDouble(args[0]);
+            teste.fSeno(a1);
+            teste.fCoseno(a1);
+            teste.fTangente(a1);
+            teste.fCotangente(a1);
+        }
+	
+	        System.out.println ("Digite uma medida em graus do angulo:");
+            String line = input.nextLine ();    // string de leitura da linha
+            
+            while (line != null)    // loop de enquando houver linhas a serem lidas
+            {
+                    System.out.println ("Digite uma medida em graus do angulo:");  // pede novamente outra string
+                    line = input.nextLine ();
+            	    if (Objects.equals(line, ""))   // caso o usuario tecle 'enter' sem digitar outros caracteres 
+            	    {
+            	        line = null;    // atribui 'null' a linha e assim encerra o loop
+            	    }	 
+            }
+	}
+}
