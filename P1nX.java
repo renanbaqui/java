19a84ef08511018234673d451754366ae1c622e6
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 11:21:29 -0300

    Update P1nX.java

--- P1nX.java
+++ P1nX.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 import java.math.RoundingMode;	// arredondamento de casas decimais
 import java.text.DecimalFormat;	// arredondamento de casas decimais
 
-public class P1nX
+public class Main
 {
  
   private static DecimalFormat df2 = new DecimalFormat("#.##"); // objeto de formato de duas casas decimais
@@ -22,6 +22,42 @@ public class P1nX
     double resultado = a * b;
     System.out.println ("A area do retangulo e': " + df2.format(resultado) + " unidades de area.");
     return resultado;     
+  }
+  
+  private static double calcula(double l1, double l2, double l3)
+  {
+    double s, area = 0;
+    int ok;
+		
+    if (l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1) // verifica se e' triangulo
+    {    
+      ok = 0; // triangulo invalido
+      System.out.println("Nao e' triangulo.");
+    }
+    else
+    {
+      ok = 1; // triangulo valido
+    }
+	 
+    if(ok==1)
+    {
+      s = (l1+l2+l3)/2;
+	  area = Math.sqrt(s*(s-l1)*(s-l2)*(s-l3));
+	  System.out.println("A area do triangulo e': "+ df2.format(area) +" unidades de area.");
+	    
+	  // Verifica triangulo equilatero
+      if (l1 == l2 && l2 == l3 )
+      System.out.println("O triangulo e' equilatero.");
+ 
+      // Verifica triangulo isosceles
+      else if (l1 == l2 || l2 == l3 || l3 == l2 )
+      System.out.println("O triangulo e' isosceles.");
+ 
+      // Else triangulo escaleno
+      else
+      System.out.println("O triangulo e' escaleno.");
+	}
+	return area;
   }
 	
   public static void main (String[]args)
@@ -49,60 +85,25 @@ public class P1nX
       {
         System.out.println((i+1)+"o argumento, “"+ args[i] +"”, nao eh numero");
       }
-     
-
-    private static double calcula(double l1, double l2, double l3)
-    {
-      double p, area;
-      int ok;
-      l1 = 113.0;
-      l2 = 4.0;
-      l3 = 5.0;
-		
-      if (l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1){    // verifica se e' triangulo
-        ok = 0; // triangulo invalido
-        System.out.println("Nao e' triangulo.");
-      }
-      else{
-        ok = 1; // triangulo valido
-      }
-	 
-      if(ok==1)
-      {
-        p = (l1+l1+l3)/2;
-	area = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
-	System.out.println("A area do triangulo e': "+ df2.format(area) +" unidades de area.");
-	    
-	// Verifica triangulo equilatero
-        if (l1 == l2 && l2 == l3 )
-        System.out.println("O triangulo e' equilatero.");
- 
-        // Verifica triangulo isosceles
-        else if (l1 == l2 || l2 == l3 || l3 == l2 )
-        System.out.println("O triangulo e' isosceles.");
- 
-        // Else triangulo escaleno
-        else
-        System.out.println("O triangulo e' escaleno.");
-	}
     }
+
     if (args.length==1)
     {
-        double conv = Double.parseDouble(args[0]); 	// conversao de string para double
-	calcula(conv);	     
+      double a1 = Double.parseDouble(args[0]); 	// conversao de string para double
+	  calcula(a1);	     
     }	
     if (args.length==2)
     {
-        double conv1 = Double.parseDouble(args[0]);	// conversao de string para double
-	double conv2 = Double.parseDouble(args[1]);
-	calcula(conv1, conv2);
+      double b1 = Double.parseDouble(args[0]);	// conversao de string para double
+	  double b2 = Double.parseDouble(args[1]);
+	  calcula(b1, b2);
     }
-    if (args.length==2)
+    if (args.length==3)
     {  
-        double conv1 = Double.parseDouble(args[0]);	// conversao de string para double
-	double conv2 = Double.parseDouble(args[1]);
-	double conv3 = Double.parseDouble(args[2]);		
-	calcula(conv1, conv2, conv3);
+      double c1 = Double.parseDouble(args[0]);	// conversao de string para double
+	  double c2 = Double.parseDouble(args[1]);
+	  double c3 = Double.parseDouble(args[2]);		
+	  calcula(c1, c2, c3);
     }	        
   }
 }
