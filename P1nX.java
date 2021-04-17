a79c5c628903931d1abf6b9b5d864fba44e3cfc5
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 11:00:28 -0300

    Update P1nX.java

--- P1nX.java
+++ P1nX.java
@@ -53,33 +53,38 @@ public class P1nX
 
     private static double calcula(double l1, double l2, double l3)
     {
-        // check condition
-		int a, b, c, ok;
-		a = 4;
-		b = 5;
-		c = 5;
-        if (a + b <= c || a + c <= b || b + c <= a){
-            ok = 0; // triangulo invalido
-        }
-        else{
-            ok = 1; // triangulo valido
-        }
-	   
+      double p, area;
+      int ok;
+      l1 = 113.0;
+      l2 = 4.0;
+      l3 = 5.0;
+		
+      if (l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1){    // verifica se e' triangulo
+        ok = 0; // triangulo invalido
+        System.out.println("Nao e' triangulo.");
+      }
+      else{
+        ok = 1; // triangulo valido
+      }
+	 
+      if(ok==1)
+      {
+        p = (l1+l1+l3)/2;
+	area = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
+	System.out.println("A area do triangulo e': "+ df2.format(area) +" unidades de area.");
+	    
 	// Verifica triangulo equilatero
-        if (a == b && b == c )
+        if (l1 == l2 && l2 == l3 )
         System.out.println("O triangulo e' equilatero.");
  
-          // Verifica triangulo isosceles
-          else if (a == b || b == c || c == b )
-          System.out.println("O triangulo e' isosceles.");
+        // Verifica triangulo isosceles
+        else if (l1 == l2 || l2 == l3 || l3 == l2 )
+        System.out.println("O triangulo e' isosceles.");
  
-          // Else triangulo escaleno
-          else
-          System.out.println("O triangulo e' escaleno.");
-	  p= (l1+l1+l3)/2;
-	  area = Math.sqrt(p*(p-l1)(p-l2)(p-l3))			
-		    
-	  System.out.println(ok);
+        // Else triangulo escaleno
+        else
+        System.out.println("O triangulo e' escaleno.");
+	}
     }
     if (args.length==1)
     {
