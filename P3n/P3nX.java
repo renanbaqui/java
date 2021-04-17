cf747274a621b5dfdb9a968db251655ba0e69450
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 20:24:40 -0300

    Create P3nX.java

---
+++ P3n/P3nX.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Objects;
+
+public class P3nX
+{
+	public static void main(String[] args) {
+	    
+	   AnguloObj teste = new AnguloObj(120);
+	   System.out.println(teste.toString());
+	}
+}
