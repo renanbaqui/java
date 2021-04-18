8d0c68b3e59c6ee59a69ff9c56f0ac601ff84746
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 21:36:29 -0300

    Update Angulo.java

--- P2n/Angulo.java
+++ P2n/Angulo.java
@@ -1,4 +1,4 @@
-import java.lang.Math.*;
+import java.lang.Math.*;	// matematica
 
 import java.math.RoundingMode;	// arredondamento de casas decimais
 import java.text.DecimalFormat;	// arredondamento de casas decimais
@@ -8,13 +8,13 @@ public class Angulo
 	
 	public static DecimalFormat df2 = new DecimalFormat("#.##"); // objeto de formato de duas casas decimais
 	
-	public static double cvtAngulo(double graus)	// metodo de calculo da area do circulo
+	public static double cvtAngulo(double graus)	// metodo de conversao de graus para radianos
 	{
 	    double radianos = (graus * Math.PI)/180;
 	    return radianos;
 	}
 	
-	public static double fSeno(double graus)	// metodo de calculo da area do circulo
+	public static double fSeno(double graus)	// metodo de calculo do seno
 	{
 	    double radianos = (graus * Math.PI)/180;
 	    double seno = Math.sin(radianos);
@@ -22,7 +22,7 @@ public class Angulo
 	    return seno;
 	}
 	
-	public static double fCoseno(double graus)	// metodo de calculo da area do circulo
+	public static double fCoseno(double graus)	// metodo de calculo do cosseno
 	{
 	    double radianos = (graus * Math.PI)/180;
 	    double coseno = Math.cos(radianos);
@@ -30,7 +30,7 @@ public class Angulo
 	    return coseno;
 	}
 	
-	public static double fTangente(double graus)	// metodo de calculo da area do circulo
+	public static double fTangente(double graus)	// metodo de calculo da tangente
 	{
 	    double radianos = (graus * Math.PI)/180;
 	    double tangente = Math.tan(radianos);
@@ -38,15 +38,12 @@ public class Angulo
 	    return tangente;
 	}
 	
-	public static double fCotangente(double graus)	// metodo de calculo da area do circulo
+	public static double fCotangente(double graus)	// metodo de calculo da cotangente
 	{
 	    double radianos = (graus * Math.PI)/180;
 	    double cotangente = (Math.cos(radianos))/(Math.sin(radianos));
 	    System.out.println("Cotangente : " + df2.format(cotangente) + "\n");
 	    return cotangente;
 	}
-	
-	public static void main(String[] args) {
-	//	System.out.println(df2.format(fSeno(30)));
-	}
+
 }
