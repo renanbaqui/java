77bf1bc653625ca2eaeaf0f61bb004d7500a263e
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-17 21:15:23 -0300

    Update P3nX.java

--- P3n/P3nX.java
+++ P3n/P3nX.java
@@ -1,11 +1,36 @@
 import java.util.Scanner;
 import java.util.Objects;
 
-public class P3nX
+public class Main
 {
 	public static void main(String[] args) {
 	    
-	   AnguloObj teste = new AnguloObj(120);
+	   Scanner input = new Scanner(System.in);
+	   int n;
+	   AnguloObj medidas[] = new AnguloObj[10];
+	   
+	   System.out.println("Digite o numero de angulos:");
+	   n = input.nextInt();
+	   
+	   System.out.println("");
+	   
+	   for (int i = 0; i < n; i++)
+	   {
+	       System.out.println("Digite a medida em graus do " + (i+1) +"o angulo:");
+	       medidas[i] = new AnguloObj(input.nextInt());
+	   }
+	   
+	   System.out.println("");
+	   
+	   System.out.println("Resultado =====================");
+	   
+	   for (int i = 0; i < n; i++)
+	   {
+	       System.out.println(medidas[i].toString());
+	   }
+	   
+/*	   AnguloObj teste = new AnguloObj(120);
 	   System.out.println(teste.toString());
+*/
 	}
 }
