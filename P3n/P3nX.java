49f58eddb50730a0fed8a742951ae2fd133669bd
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-19 02:32:02 -0300

    Update P3nX.java

--- P3n/P3nX.java
+++ P3n/P3nX.java
@@ -3,28 +3,62 @@ import java.util.Objects;
 
 public class Main
 {
+	public static void verifica(String s)   // metodo de verificacao de numero
+	{
+    	boolean numeric = true;
+    
+		try {	// verifica se e' numero
+		  Double num = Double.parseDouble(s);
+		} catch (NumberFormatException e) {
+		  numeric = false;
+		}
+        
+		if (!numeric)	// se nao e' numero imprime a linha abaixo e sai do programa
+		{
+		  System.out.println("O argumento “"+ s +"” nao eh numero");
+		  System.exit(0);
+		}    
+	}
+	
+	
 	public static void main(String[] args) {
 	    
+	   if (args.length>=1) // se o numero de argumentos for igual ou maior a um
+	   {
+	     System.out.println("Numero de argumentos excessivo");
+		 System.exit(0);
+	   }	
+	   
 	   Scanner input = new Scanner(System.in);
-	   int n;
+	   String n;
 	   AnguloObj medidas[] = new AnguloObj[10];	// criacao de array de objetos da classe AnguloObj, onde serao armazenados os objetos criados
 	   
 	   System.out.println("Digite o numero de angulos:");
-	   n = input.nextInt();
+	   n = input.nextLine();
+	   verifica(n);
+	   int m = Integer.parseInt(n);
+	   if(m<1 || m>10)
+	   {
+	     System.out.println("Numero de angulos invalido. Numero deve ser entre 1 e 10 inclusive.");	// tratamento de excessao do numero de angulos
+		 System.exit(0);
+	   }
 	   
 	   System.out.println("");
 	   
-	   for (int i = 0; i < n; i++)	// loop para iterar pelo array para atribuir as medidas dos angulos ao array de objetos da classe AnguloObj
+	   for (int i = 0; i < m; i++)	// loop para iterar pelo array para atribuir as medidas dos angulos ao array de objetos da classe AnguloObj
 	   {
 	       System.out.println("Digite a medida em graus do " + (i+1) +"o angulo:");
-	       medidas[i] = new AnguloObj(input.nextInt());	// atribui ao array 'medidas' no indice i o objeto da classe AnguloObj com sua respectiva medida em graus
+	       String line = input.nextLine();
+	       verifica(line);
+	       double convertido = Double.parseDouble(line);   // converte para double
+	       medidas[i] = new AnguloObj(convertido);	// atribui ao array 'medidas' no indice i o objeto da classe AnguloObj com sua respectiva medida em graus
 	   }
 	   
 	   System.out.println("");
 	   
 	   System.out.println("Resultado =====================");
 	   
-	   for (int i = 0; i < n; i++)	// loop iterar a impressao do resultado
+	   for (int i = 0; i < m; i++)	// loop para iterar a impressao do resultado
 	   {
 	       System.out.println(medidas[i].toString());	// impressao do resultado de cada indice do array de objetos da classe AnguloObj
 	   }
