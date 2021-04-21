79e57fa085a670047e077db9e5f8fe4b109cd9a8
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-21 14:05:42 -0300

    Create P4nX.java

---
+++ P4n/P4nX.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Objects;
+
+public class P3nX
+{
+    
+    
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        int c, e;
+        
+        System.out.println("Quantos Contratados?");
+        c = input.nextInt();
+        
+        System.out.println("Quantos Executivos?");
+        e = input.nextInt();
+        
+        calculaSalarios(); //???
+    }
+}
