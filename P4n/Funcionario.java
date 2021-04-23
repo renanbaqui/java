931478a0d7ae6b543ce9019a250d340fdd974ae4
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-23 11:35:08 -0300

    Update Funcionario.java

--- P4n/Funcionario.java
+++ P4n/Funcionario.java
@@ -12,15 +12,18 @@ public class Funcionario
         this.salarioLiquido = s;
     }
     
-    double calculaSalario(double desconto)
+    float calculaSalario(float desconto)
     {
-        this.salarioLiquido = this.salario * (100 - desconto)/100;
+        return this.salario * (100 - desconto)/100;
     }
     
     public String toString()
     {
     
-        return "retorna o nome, código e o salário-base do empregado";
+        return this.nome + "\n" +
+        this.codigo+ "\n" +
+        this.salario; // salarioBase?
+        ;
     }
     
 }
