82ba74d7d2834bb18763e0685a4f72ca81aa7888
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-25 11:45:18 -0300

    Update FuncCntrd.java

--- P4n/FuncCntrd.java
+++ P4n/FuncCntrd.java
@@ -2,30 +2,30 @@ public class FuncCntrd extends Funcionario
 {
     int dependentes;
     float salarioFamilia;
-    final float valorPorDep = 9.58, aliquotaIR = 15.00;
+    final float valorPorDep = 9.58f, aliquotaIR = 15.00f;
     
     public FuncCntrd(String a, String b, float d, int i)
     {
-        Funcionario(a, b, d); // this. ??
+        super(a, b, d);
+  //      Funcionario(a, b, d); // this. ??
         this.dependentes = i;
     }
     
     float calculaSalario()
     {
-        return Funcionario.calculaSalario(aliquotaIR); // this.?
+        return super.calculaSalario(aliquotaIR); // this.?
     }
     
     float calculaSalario(int numeroDependentes)
     {
         this.salarioFamilia = numeroDependentes * valorPorDep; 
         float salarioBase = this.salario + this.salarioFamilia;
-        return calculaSalario();
+    //    return calculaSalario();
         return salarioBase;
     }
     
     public String toString()
     {
-    
         return this.nome + "\n" +
         this.codigo + "\n" +
         (this.salario+this.salarioFamilia) + "\n" + // salario base
