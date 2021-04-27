6071777fd703473e32f57c629efc5dee4bcc9e59
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-27 17:24:44 -0300

    Update P4nX.java

--- P4n/P4nX.java
+++ P4n/P4nX.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
-import java.util.Objects;
+//import java.util.Scanner;
+//import java.util.Objects;
 import java.util.*;
+
+import java.math.RoundingMode;	// arredondamento de casas decimais
+import java.text.DecimalFormat;	// arredondamento de casas decimais
 
 public class Main
 {
     //static ArrayList<Funcionario> teste2 = new ArrayList<Funcionario>();
     
     static ArrayList<Funcionario> teste;
-    static int f;
+    static String f;
+    static int m;
+    
+    public static DecimalFormat df2 = new DecimalFormat("#.##"); // objeto de formato de duas casas decimais
     
     //ArrayList<Funcionario> teste;
     
@@ -20,24 +26,81 @@ public class Main
         
     }
     
+    public static void verificaInt(String s)   // metodo de verificacao de numero inteiro
+	{
+    	boolean numeric = true;
+    
+		try {	// verifica se e' numero inteiro
+		  Integer num = Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+		  numeric = false;
+		}
+        
+		if (!numeric)	// se nao e' numero imprime a linha abaixo e sai do programa
+		{
+		  System.out.println("O argumento “"+ s +"” nao eh numero inteiro.");
+		  System.exit(0);
+		}    
+	}
+	
+	public static void verificaNumero(int n)
+	{
+	    if (n<1 || n>10)
+	    {
+	        System.out.println("Numero de funcionarios invalido. Numero deve ser entre 1 e 10 inclusive.");
+		    System.exit(0);
+	    }
+	}
+    
+    public static void verificaCE(String s)
+    {
+        if(!s.equals("c") && !s.equals("e"))
+        {
+            System.out.println("O argumento “"+ s +"” nao eh valido. Insira 'c' ou 'e'.");
+		    System.exit(0);
+        }
+    }
+    
+    public static void verificaFloat(String s)   // metodo de verificacao de numero double
+	{
+    	boolean numeric = true;
+    
+		try {	// verifica se e' numero double
+		  Float num = Float.parseFloat(s);
+		} catch (NumberFormatException e) {
+		  numeric = false;
+		}
+        
+		if (!numeric)	// se nao e' numero imprime a linha abaixo e sai do programa
+		{
+		  System.out.println("O argumento “"+ s +"” nao eh numero valido.");
+		  System.exit(0);
+		}    
+	}
+    
     public static void entDados()
     {
         
         Scanner entrada = new Scanner(System.in);
         
         
-        for (int i = 0; i < f; i++)
+        for (int i = 0; i < m; i++)
         {
             System.out.println("Contratado ou Executivo (c/e)?");
             String ce = entrada.nextLine();
+            verificaCE(ce);
             System.out.println("Nome do Empregado:");
             String a = entrada.nextLine();
             System.out.println("Codigo:");
             String b = entrada.nextLine();
             System.out.println("Salario:");
-            float c = entrada.nextFloat();
+            String x = entrada.nextLine();
+            verificaFloat(x);
+            float c = Float.parseFloat(x);
             System.out.println("Numero de Dependentes:");
-            int d = entrada.nextInt();
+            String y = entrada.nextLine();
+            verificaInt(y);
+            int d = Integer.parseInt(y);
             
             switch(ce) {
               case "c":
@@ -46,7 +109,9 @@ public class Main
                 break;
               case "e":
                 System.out.println("Gratificacao:");
-                float e = entrada.nextFloat();
+                String z = entrada.nextLine();
+                verificaFloat(z);
+                float e = Float.parseFloat(z);
                 FuncionarioExct f2 = new FuncionarioExct(a, b, c, d, e);
                 teste.add(f2);
                 break;
@@ -86,9 +151,9 @@ public class Main
             System.out.println("Salario-Base:");
             st.calculaSalario(st.dependentes);
         //    System.out.println(st.calculaSalario(st.dependentes)); // ao rodar o metodo atribui novo valor ao campo salario
-            System.out.println(st.salario);
+            System.out.println(df2.format(st.salario));
             System.out.println("Salario-Liquido:");
-            System.out.println(st.calculaSalario());
+            System.out.println(df2.format(st.calculaSalario()));
             System.out.println("----");
         }  
     /*    System.out.println("Nome:");
@@ -105,16 +170,24 @@ public class Main
     
     public static void main(String[] args) {
     
+        if (args.length>=1) // se o numero de argumentos for igual ou maior a um
+	    {
+	        System.out.println("Numero de argumentos excessivo.");
+		    System.exit(0);
+	    }	
+        
         Scanner input = new Scanner(System.in);
         
        // String ce;
       //  ArrayList<FuncionarioExct> teste = new ArrayList<FuncionarioExct>(); // Funcionarios ??? 
         
-        System.out.println("Quantos Funcionarios?");    // pedir ao usuário o número de objetos (FuncCntrd e FuncExct) 
-        f = input.nextInt();                            // para os quais o salário vai ser calculado;
-        //verifica(f);
+        System.out.println("Quantos Funcionarios?");        // pedir ao usuário o número de objetos (FuncCntrd e FuncExct) 
+        f = input.nextLine();                               // para os quais o salário vai ser calculado;
+        verificaInt(f);
+        m = Integer.parseInt(f);	// converte para tipo de dados int
+        verificaNumero(m);
         
-        Main p = new Main(f);   // objeto instanciado da classe principal
+        Main p = new Main(m);   // objeto instanciado da classe principal
         
         System.out.println("--- Cadastro de Funcionarios");
 //        
