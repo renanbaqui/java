7c25b1c133e0dff837272cf1a3f0b5150c515ff3
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-27 21:12:49 -0300

    Update Funcionario.java

--- P4n/Funcionario.java
+++ P4n/Funcionario.java
@@ -4,18 +4,22 @@ public class Funcionario
     String nome, codigo;
     float salario, salarioLiquido;
     
-    public Funcionario(String n, String c, float s)
+    public Funcionario(String n, String c, float s) // construtor da classe Funcionario
     {
+        
         this.nome = n;
         this.codigo = c;
         this.salario = s;
         this.salarioLiquido = s;
+        
     }
     
-    float calculaSalario(float desconto)
+    float calculaSalario(float desconto)    // metodo que retorna o valor do salario liquido a ser recebido pelo empregado
     {
+        
         this.salarioLiquido = salario * (100 - desconto)/100;
         return salarioLiquido;
+        
     }
     
     // métodos getXXX para cada campo da classe
@@ -28,11 +32,13 @@ public class Funcionario
     
     public float getSalarioLiquido() { return this.salarioLiquido; }
     
-    public String toString()
+    public String toString()    // metodo toString que retorna o nome, codigo e o salario base do empregado
     {
+        
         return this.nome + "\n" +
         this.codigo + "\n" +
         this.salario; // salarioBase?
+        
     }
     
 }
