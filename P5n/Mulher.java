27e803369891f6a223720621ebc5b4de6134fc32
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-05-01 14:18:40 -0300

    Create Mulher.java

---
+++ P5n/Mulher.java
@@ -0,0 +1,34 @@
+import java.math.RoundingMode;	// arredondamento de casas decimais
+import java.text.DecimalFormat;	// arredondamento de casas decimais
+
+public class Mulher extends PessoaIMC
+{
+    
+    public static DecimalFormat df2 = new DecimalFormat("#.##");	// objeto de formato de duas casas decimais
+    
+    Mulher(String n, String d, double p, double a)
+    {
+        super(n, d, p, a);
+    }
+    
+    public String resultIMC()
+    {
+        double imc = super.calculaIMC(super.getAltura(), super.getPeso());
+        
+        if(imc<19)
+            return "Abaixo do peso ideal.";
+        
+        else if(imc>=19 && imc<=25.8)
+            return "Peso ideal.";
+            
+        else
+            return "Acima do peso ideal.";
+    }
+    
+    public String toString()
+    {
+        return  super.toString() + "\n" +
+                "IMC: " + df2.format(super.calculaIMC(super.getAltura(), super.getPeso())) + "\n" + 
+                resultIMC(); 
+    }
+}
