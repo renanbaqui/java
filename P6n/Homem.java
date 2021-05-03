e00cee466585813d6a485ed64f808d3e9df0e1b4
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-05-03 20:03:18 -0300

    Add files via upload

---
+++ P6n/Homem.java
@@ -0,0 +1,34 @@
+import java.math.RoundingMode;	// arredondamento de casas decimais
+import java.text.DecimalFormat;	// arredondamento de casas decimais
+
+public class Homem extends PessoaIMC
+{
+    
+    public static DecimalFormat df2 = new DecimalFormat("#.##");	// objeto de formato de duas casas decimais
+    
+    Homem(String n, String d, double p, double a)
+    {
+        super(n, d, p, a);
+    }
+    
+    public String resultIMC()
+    {
+        double imc = super.calculaIMC(super.getAltura(), super.getPeso());
+        
+        if(imc<20.7)
+            return "Abaixo do peso ideal.";
+        
+        else if(imc>=20.7 && imc<=26.4)
+            return "Peso ideal.";
+            
+        else
+            return "Acima do peso ideal.";
+    }
+    
+    public String toString()
+    {
+        return  super.toString() + "\n" +
+                "IMC: " + df2.format(super.calculaIMC(super.getAltura(), super.getPeso())) + "\n" + 
+                resultIMC(); 
+    }
+}
