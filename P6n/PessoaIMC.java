360f3dcbf42fe59c70582cdb6b816a1d247c7e9b
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-05-03 20:03:40 -0300

    Add files via upload

---
+++ P6n/PessoaIMC.java
@@ -0,0 +1,31 @@
+abstract class PessoaIMC extends Pessoa
+{
+    private double peso, altura;
+    
+    PessoaIMC(String n, String d, double p, double a)
+    {
+        super(n, d);
+        this.peso = p;
+        this.altura = a;
+    }
+    
+    public double getPeso(){ return this.peso; }
+    
+    public double getAltura(){ return this.altura; }
+    
+    double calculaIMC(double a, double p)
+    {
+        double imc = p / (a * a);
+        return imc;
+    }
+    
+    // Abstract method (does not have a body)
+//    public abstract String resultIMC(); // esta certo?
+    
+        public String toString()
+    {
+        return  super.toString() + "\n" +
+                "Peso: " + this.peso + "\n" +
+                "Altura: " + this.altura;
+    }
+}   
