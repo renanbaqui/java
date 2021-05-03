e2f01f0a6eba4857ac8708f018f1ff54e01a94ab
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-05-03 20:13:31 -0300

    Create P6nX.java

---
+++ P6n/P6nX.java
@@ -0,0 +1,23 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		MinhaListaOrdenavel objeto = new MinhaListaOrdenavel //?
+		
+		switch(hm) {
+        	case "h":
+    	    	Homem p1 = new Homem(a, b, c, d);
+    	    	lista.add(p1);
+    	    	break;
+    	    case "m":
+    	        Mulher p2 = new Mulher(a, b, c, d);
+    	    	lista.add(p2);
+    	    	break;
+        }
+        
+        // Na sequência crie um menu para o usuário imprimir a lista de produtos ou sair do
+        // programa. Se o usuário optar por listar, pergunte qual o critério e liste os produtos.
+		
+	}
+}
